e8a293fd7b8b81b74fbf786d54e404c97b3aaa27
Mor Ndiaye <morn5574@gmail.com>
2025-08-26 15:14:19 +0000

    fichier jenkins

--- src/test/java/com/groupeisi/com/dondesang_sn/DonDeSangSnApplicationTests.java
+++ src/test/java/com/groupeisi/com/dondesang_sn/DonDeSangSnApplicationTests.java
@@ -3,11 +3,17 @@ package com.groupeisi.com.dondesang_sn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class DonDeSangSnApplicationTests {
 
     @Test
     void contextLoads() {
+        int num1 = 10;
+        int num2 = 20;
+        int resultat = num1 + num2;
+        assertEquals(30, resultat);
     }
 
 }
